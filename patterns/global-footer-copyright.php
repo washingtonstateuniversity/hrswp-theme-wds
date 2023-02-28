91a6260f9a5a8a946dc09a98b17c9aadfa82f6c2
Adam Turner <act@adamturner.dev>
2023-02-28 12:01:35 -0800

    Create global footer copyright pattern

---
+++ patterns/global-footer-copyright.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Global Footer Copyright
+ * Slug: hrswds/global-footer-copyright
+ * Categories: footer
+ * Block Types: hrswds/template-part/footer
+ */
+?>
+<!-- wp:paragraph -->
+<p>© Washington State University <?php echo esc_attr( date( 'Y' ) ); ?></p>
+<!-- /wp:paragraph -->
