48cff7268f72630241c52e54e6985b6def52f688
Adam Turner <act@adamturner.dev>
2023-04-05 09:28:41 -0700

    Replace footer logo pattern

--- patterns/site-footer-content.php
+++ patterns/site-footer-content.php
@@ -11,5 +11,8 @@
 namespace HRSWP\Theme\WDS\Patterns\SiteFooterContent;
 
 use HRSWP\Theme\WDS\Inc\Classes\Class_SVG_Icons;
+?>
 
-echo Class_SVG_Icons\SVG_Icons::get_svg( 'cougar', 48, 'wsu-coug-head wsu-coug-head--white' );
+<!-- wp:paragraph -->
+<p>Site footer logo</p>
+<!-- /wp:paragraph -->
