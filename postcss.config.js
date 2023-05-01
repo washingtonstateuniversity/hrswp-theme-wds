60c2ba3ccb80a4140c93c6eba92e1c68865142ee
Adam Turner <act@adamturner.dev>
2023-05-01 09:33:30 -0700

    Close #9 set up CSS build tools and loader

---
+++ postcss.config.js
@@ -0,0 +1,38 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const plugins = [
+	// eslint-disable-next-line import/no-extraneous-dependencies
+	require( 'postcss-import' )( {
+		path: 'src/',
+	} ),
+	// eslint-disable-next-line import/no-extraneous-dependencies
+	require( 'postcss-preset-env' )( {
+		stage: 2,
+		autoprefixer: {
+			grid: true,
+		},
+		features: {
+			'nesting-rules': true,
+		},
+	} ),
+];
+
+module.exports = {
+	ident: 'postcss',
+	sourceMap: ! isProduction,
+	plugins: isProduction
+		? [
+			...plugins,
+			// eslint-disable-next-line import/no-extraneous-dependencies
+			require( 'cssnano' )( {
+				preset: [
+					'default',
+					{
+						discardComments: {
+							removeAll: true,
+						},
+					},
+				],
+			} ),
+		]
+		: plugins,
+};
