81be8dd8952f0afda4900dcc96defc3c060e29c3
Adam Turner <act@adamturner.dev>
2023-07-07 10:43:48 -0700

    :package: Update built files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '64e9033492a252df12f3');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3a423b93dcf4449b755f');
