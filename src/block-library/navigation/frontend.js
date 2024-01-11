3025e004709164ce2bdb618aa809e6ae78dc7675
Adam Turner <act@adamturner.dev>
2024-01-11 11:36:04 -0800

    Fix #80 site nav submenus should stay open with current page

--- src/block-library/navigation/frontend.js
+++ src/block-library/navigation/frontend.js
@@ -1,1 +1,37 @@
-export default {};
+/**
+ * Navigation menu modifiers
+ */
+
+/**
+ * Sets submenu with current page to expanded.
+ *
+ * Using setTimeout in order to not be overwritten by WP Core navigation view
+ * script.
+ *
+ * @param {Node} navigationBlock
+ */
+function setSubmenuActive( navigationBlock ) {
+	const currentMenuTarget = navigationBlock.querySelector(
+		'.current-menu-ancestor ~ button'
+	);
+
+	if ( currentMenuTarget ) {
+		setTimeout(
+			() => currentMenuTarget.setAttribute( 'aria-expanded', true ),
+			100
+		);
+	}
+}
+
+/**
+ * Initializes the navigation modifers.
+ */
+function init() {
+	const siteNavigationBlock = document.querySelector( 'nav.site-navigation' );
+
+	if ( siteNavigationBlock ) {
+		setSubmenuActive( siteNavigationBlock );
+	}
+}
+
+init();
