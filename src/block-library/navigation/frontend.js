d79e3e8e24fc1fbe55f850e0488edd36f4b986b6
Adam Turner <act@adamturner.dev>
2024-02-05 11:19:27 -0800

    [WIP] Quicklinks/search menu controls

--- src/block-library/navigation/frontend.js
+++ src/block-library/navigation/frontend.js
@@ -8,6 +8,14 @@ class SiteNavigation {
 		this.navigationScrim = document.querySelector(
 			'.wsu-navigation-menu--scrim'
 		);
+
+		this.openSearchTargets = document.querySelectorAll(
+			'.quicklinks-panel-toggle .wp-element-button'
+		);
+		this.closeSearchTarget = document.querySelector(
+			'.search-menu--close .hrswds-svg-icon-container'
+		);
+
 		this.menuControls = {
 			menuToggle: document.querySelector( 'button.open-site-navigation' ),
 			closeMenuTarget: document.querySelector(
@@ -18,15 +26,37 @@ class SiteNavigation {
 		this.openMenu = this.openMenu.bind( this );
 		this.closeMenu = this.closeMenu.bind( this );
 		this.toggleMenu = this.toggleMenu.bind( this );
+
+		this.openSearchMenu = this.openSearchMenu.bind( this );
+		this.closeSearchMenu = this.closeSearchMenu.bind( this );
+
 		this.setSubmenuActive = this.setSubmenuActive.bind( this );
 
-		this.initCloseMenuButton();
+		this.openSearchTargets.forEach( ( target ) => {
+			this.initButton(
+				target,
+				'Open search menu',
+				'quicklinks-search-menu'
+			);
+		} );
+
+		this.initButton(
+			this.menuControls.closeMenuTarget,
+			'Close site navigation',
+			'site-navigation-parent'
+		);
+		this.initButton(
+			this.closeSearchTarget,
+			'Close search menu',
+			'quicklinks-search-menu'
+		);
+
 		this.addEventListeners();
 		this.setSubmenuActive();
 	}
 
 	openMenu() {
-		this.sidebarWrapper.classList.add( 'show-sidebar' );
+		this.sidebarWrapper.classList.add( 'show-sidebar', 'show-scrim-menu' );
 
 		Object.values( this.menuControls ).forEach( ( control ) => {
 			control.setAttribute( 'aria-expanded', 'true' );
@@ -38,13 +68,32 @@ class SiteNavigation {
 	}
 
 	closeMenu() {
-		this.sidebarWrapper.classList.remove( 'show-sidebar' );
+		this.sidebarWrapper.classList.remove(
+			'show-sidebar',
+			'show-scrim-menu'
+		);
 
 		Object.values( this.menuControls ).forEach( ( control ) => {
 			control.setAttribute( 'aria-expanded', 'false' );
 			control.setAttribute( 'aria-label', 'Open site navigation' );
 			control.setAttribute( 'title', 'Open site navigation' );
 		} );
+	}
+
+	openSearchMenu() {
+		const control = document.getElementById(
+			this.closeSearchTarget.getAttribute( 'aria-controls' )
+		);
+		control.classList.add( 'show-menu' );
+		this.sidebarWrapper.classList.add( 'show-scrim-search' );
+	}
+
+	closeSearchMenu() {
+		const control = document.getElementById(
+			this.closeSearchTarget.getAttribute( 'aria-controls' )
+		);
+		control.classList.remove( 'show-menu' );
+		this.sidebarWrapper.classList.remove( 'show-scrim-search' );
 	}
 
 	toggleMenu() {
@@ -55,13 +104,18 @@ class SiteNavigation {
 		}
 	}
 
-	initCloseMenuButton() {
-		const menuButton = this.menuControls.closeMenuTarget;
-
-		menuButton.setAttribute( 'title', 'Close site navigation' );
-		menuButton.setAttribute( 'aria-label', 'Close site navigation' );
-		menuButton.setAttribute( 'aria-expanded', 'false' );
-		menuButton.setAttribute( 'aria-controls', 'site-navigation-parent' );
+	/**
+	 * Sets up a button target.
+	 *
+	 * @param {Element} button
+	 * @param {string}  label
+	 * @param {string}  controls
+	 */
+	initButton( button, label, controls ) {
+		button.setAttribute( 'aria-expanded', 'false' );
+		button.setAttribute( 'title', label );
+		button.setAttribute( 'aria-label', label );
+		button.setAttribute( 'aria-controls', controls );
 	}
 
 	addEventListeners() {
@@ -73,6 +127,15 @@ class SiteNavigation {
 			'click',
 			this.closeMenu
 		);
+
+		this.openSearchTargets.forEach( ( target ) => {
+			target.addEventListener( 'click', this.openSearchMenu );
+		} );
+		this.closeSearchTarget.addEventListener(
+			'click',
+			this.closeSearchMenu
+		);
+
 		document.addEventListener( 'keyup', ( event ) => {
 			if ( this._isMenuOpen() && event.key === 'Escape' ) {
 				this.closeMenu();
