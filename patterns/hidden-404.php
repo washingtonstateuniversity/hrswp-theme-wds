4ad31bd18832787b6813290a9b9d15e28ce9877d
Adam Turner <act@adamturner.dev>
2024-02-12 11:42:26 -0800

    Replace static list with quicklinks nav menu

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -17,35 +17,16 @@ namespace HRSWP\Theme\WDS\Patterns\Hidden404;
 	<!-- /wp:heading -->
 </header>
 <!-- /wp:group -->
-
 <!-- wp:paragraph -->
 <p><?php echo esc_html_x( 'We\'re sorry, but the page you are looking for could not be found. Please try our site search or one of the following links.', 'Message to convey that a webpage could not be found', 'hrswp-theme-wds' ); ?></p>
 <!-- /wp:paragraph -->
-
 <!-- wp:group {"tagName":"section","align":"full","backgroundColor":"secondary-light","layout":{"type":"constrained"},"metadata":{"name":"Search"}} -->
 <section class="wp-block-group alignfull has-secondary-light-background-color has-background">
 	<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search","buttonText":"Search","buttonUseIcon":true} /-->
 </section>
 <!-- /wp:group -->
-
-<!-- wp:list -->
-<ul>
-	<!-- wp:list-item -->
-	<li><a href="https://index.wsu.edu"><?php esc_html_e( 'The A-Z Directory', 'hrswp-theme-wds' ); ?></a> <?php esc_html_e( '(an alphabetical list of departments, programs, resources, organizations, and more)', 'hrswp-theme-wds' ); ?></li>
-	<!-- /wp:list-item -->
-	<!-- wp:list-item -->
-	<li><a href="https://wsu.edu/academics/"><?php esc_html_e( 'Academic Departments', 'hrswp-theme-wds' ); ?></a> <?php esc_html_e( '(a list of links to all academic departments)', 'hrswp-theme-wds' ); ?></li>
-	<!-- /wp:list-item -->
-	<!-- wp:list-item -->
-	<li><a href="https://wsu.edu/about/services/"><?php esc_html_e( 'Administrative Offices and Resources', 'hrswp-theme-wds' ); ?></a> <?php esc_html_e( '(a list of links to all administrative offices and resources)', 'hrswp-theme-wds' ); ?></li>
-	<!-- /wp:list-item -->
-	<!-- wp:list-item -->
-	<li><a href="https://wsu.edu/admission/"><?php esc_html_e( 'Admissions and Aid', 'hrswp-theme-wds' ); ?></a> <?php esc_html_e( '(information for prospective students about applying, tuition, financial aid, and more)', 'hrswp-theme-wds' ); ?></li>
-	<!-- /wp:list-item -->
-	<!-- wp:list-item -->
-	<li><a href="https://wsu.edu/contact/"><?php esc_html_e( 'Contact Us', 'hrswp-theme-wds' ); ?></a> <?php esc_html_e( '(phone and email contacts)', 'hrswp-theme-wds' ); ?></li>
-	<!-- /wp:list-item -->
-</ul>
-<!-- /wp:list -->
-
-
+<!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">
+	<?php esc_html_e( 'Quicklinks', 'hrswp-theme-wds' ); ?>
+</h2>
+<!-- /wp:heading -->
