f21202a133a08bbbf66f804f01b97a4b21069e5e
Adam Turner <act@adamturner.dev>
2024-02-15 11:31:03 -0800

    Style heading links for #46 and add inline icon for #42

--- inc/blocks.php
+++ inc/blocks.php
@@ -7,6 +7,8 @@
 
 namespace HRSWP\Theme\WDS\Inc\Blocks;
 
+use HRSWP\Theme\WDS\Inc\Classes\Class_SVG_Icons;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -58,6 +60,7 @@ add_filter(
 	 * @param WP_Block $instance      The block instance.
 	 */
 	function ( string $block_content, array $block ): string {
+		// Removes hover triggers from site navigation submenus.
 		if ( 'core/navigation' === $block['blockName'] ) {
 			$find = array(
 				'data-wp-on--mouseenter="actions.core.navigation.openMenuOnHover"',
@@ -68,8 +71,8 @@ add_filter(
 			$block_content = str_replace( $find, '', $block_content );
 		}
 
+		// Adds ARIA attributes to the site navigation menu toggle.
 		if ( 'hrswds/svg-selector' === $block['blockName'] && isset( $block['attrs']['className'] ) ) {
-
 			if ( 'wp-block-navigation__responsive-container-open' === $block['attrs']['className'] ) {
 				$p = new \WP_HTML_Tag_Processor( $block_content );
 				if ( $p->next_tag( 'button' ) ) {
@@ -82,6 +85,17 @@ add_filter(
 				}
 
 				$block_content = $p->get_updated_html();
+			}
+		}
+
+		// Adds icons to heading links.
+		if ( 'core/heading' === $block['blockName'] ) {
+			$p = new \WP_HTML_Tag_Processor( $block_content );
+			if ( $p->next_tag( 'a' ) ) {
+				$icon = Class_SVG_Icons\SVG_Icons::get_svg( 'link', 18, 'heading-link-icon' );
+				$p->add_class( 'has-inline-icon' );
+				$block_content = $p->get_updated_html();
+				$block_content = str_replace( '</a>', $icon . '</a>', $block_content );
 			}
 		}
 
