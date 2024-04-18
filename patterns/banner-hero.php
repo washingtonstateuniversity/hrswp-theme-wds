e555e0a088054f66f2d1c73b8458c290130237f6
Adam Turner <act@adamturner.dev>
2024-04-18 10:07:24 -0700

    Start hero banner pattern

---
+++ patterns/banner-hero.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Hero Banner
+ * Slug: hrswds/banner-hero
+ * Categories: banner, featured
+ * Viewport width: 1400
+ *
+ * @package HRSWP_ThemeWDS
+ */
+
+namespace HRSWP\Theme\WDS\Patterns\BannerHero;
+
+?>
+
+
+<div class="wsu-hero wsu-width--full wsu-pattern--wsu-light-radial-left wsu-hero--style-title wsu-hero--shape-notched ">
+
+	<div class="wsu-image-frame wsu-image-frame--fill">
+	<img src="https://s3.wp.wsu.edu/uploads/sites/3165/2022/11/Campus-Night-View_7754-1900x1252-1.jpg" srcset="https://s3.wp.wsu.edu/uploads/sites/3165/2022/11/Campus-Night-View_7754-1900x1252-1.jpg 1900w, https://s3.wp.wsu.edu/uploads/sites/3165/2022/11/Campus-Night-View_7754-1900x1252-1.jpg 396w, https://s3.wp.wsu.edu/uploads/sites/3165/2022/11/Campus-Night-View_7754-1900x1252-1.jpg 792w, https://s3.wp.wsu.edu/uploads/sites/3165/2022/11/Campus-Night-View_7754-1900x1252-1.jpg 768w, https://s3.wp.wsu.edu/uploads/sites/3165/2022/11/Campus-Night-View_7754-1900x1252-1.jpg 1536w" sizes="(max-width: 1900px) 100vw, 1900px" alt="" style="object-position: 52% 61%">
+</div>
+
+
+	<div class="wsu-overlay wsu-pattern-after wsu-overlay--dark-left wsu-pattern--wsu-light-radial-left"></div>
+	<div class="wsu-hero__content">
+		<div class="wsu-hero__caption">
+						<div class="wsu-eyebrow-header">
+				<strong>A classic</strong>			</div>
+									<div id="title-id-6621528eeaa06" class="wsu-title wsu-hero__title"><span><strong>College-town Experience</strong></span></div>
+											</div>
+	</div>
+</div>
