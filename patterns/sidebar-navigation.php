c231b3b2222501876b48697196e213f34e22e5a4
Adam Turner <act@adamturner.dev>
2024-04-29 11:51:17 -0700

    Create sidebar navigation pattern for #109

---
+++ patterns/sidebar-navigation.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Sidebar navigation
+ * Slug: hrswds/sidebar-navigation
+ * Categories: layout
+ *
+ * @package HRSWP_ThemeWDS
+ */
+
+namespace HRSWP\Theme\WDS\Patterns\SidebarNavigation;
+
+?>
+<!-- wp:group {"tagName":"aside","metadata":{"name":"Sidebar navigation"},"className":"sidebar-navigation","layout":{"type":"default"}} -->
+<aside class="wp-block-group sidebar-navigation">
+	<!-- wp:heading -->
+	<h2 class="wp-block-heading">Dig deeper</h2>
+	<!-- /wp:heading -->
+	<!-- wp:list {"className":"is-style-lined"} -->
+	<ul class="is-style-lined">
+		<!-- wp:list-item -->
+		<li><a href="http://workday.wsu.edu/">Workday Self-Service</a></li>
+		<!-- /wp:list-item -->
+		<!-- wp:list-item -->
+		<li><a href="https://wsu.percipio.com/">Percipio Online Learning</a></li>
+		<!-- /wp:list-item -->
+		<!-- wp:list-item -->
+		<li><a href="https://dev.hrswds.test/#">WSU Holiday Schedule</a></li>
+		<!-- /wp:list-item -->
+		<!-- wp:list-item -->
+		<li><a href="https://dev.hrswds.test/#">Employee Assistance Program</a></li>
+		<!-- /wp:list-item -->
+		<!-- wp:list-item -->
+		<li><a href="https://dev.hrswds.test/#">Life Events</a></li>
+		<!-- /wp:list-item -->
+	</ul>
+	<!-- /wp:list -->
+</aside>
+<!-- /wp:group -->
