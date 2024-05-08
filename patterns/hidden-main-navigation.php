98fe1d70d5ec41d9e60df813cef806f01d5da30e
Adam Turner <act@adamturner.dev>
2024-05-08 09:02:42 -0700

    Move audiences nav to top and remove unneeded template part

---
+++ patterns/hidden-main-navigation.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Hidden Main Navigation
+ * Slug: hrswds/hidden-main-navigation
+ * Inserter: no
+ *
+ * @package HRSWP_ThemeWDS
+ */
+
+namespace HRSWP\Theme\WDS\Patterns\HiddenMainNavigation;
+
+?>
+<!-- wp:group {"className":"main-navigation","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group main-navigation">
+	<!-- wp:paragraph {"className":"wp-block-heading"} -->
+	<p class="wp-block-heading">Resources for</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:navigation {"showSubmenuIcon":false,"overlayMenu":"never","layout":{"type":"flex","flexWrap":"nowrap"}} /-->
+</div>
+<!-- /wp:group -->
