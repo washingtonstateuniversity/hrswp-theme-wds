d6d2d329cc9fab5b7696c3138a35273c167205f8
Adam Turner <act@adamturner.dev>
2024-05-15 08:45:44 -0700

    Adjust audiences heading

--- patterns/hidden-main-navigation.php
+++ patterns/hidden-main-navigation.php
@@ -13,7 +13,7 @@ namespace HRSWP\Theme\WDS\Patterns\HiddenMainNavigation;
 <!-- wp:group {"className":"main-navigation","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"},"fontSize":"small"} -->
 <div class="wp-block-group main-navigation has-small-font-size">
 	<!-- wp:paragraph {"className":"wp-block-heading"} -->
-	<p class="wp-block-heading"><strong>Resources for</strong></p>
+	<p class="wp-block-heading"><strong>Resources for:</strong></p>
 	<!-- /wp:paragraph -->
 	<!-- wp:navigation {"showSubmenuIcon":false,"overlayMenu":"never","className":"is-style-underline-thick","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} /-->
 </div>
