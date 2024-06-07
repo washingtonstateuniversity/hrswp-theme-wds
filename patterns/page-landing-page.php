4f64ae54a189a060057107eaab5ed2cd7fb19baa
Adam Turner <act@adamturner.dev>
2024-06-07 09:10:45 -0700

    [WIP] Set up landing page template for #102

---
+++ patterns/page-landing-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Landing page
+ * Slug: hrswds/page-langing-page
+ * Categories: hrswds_page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A unified landing page with a hero bannner, etc..
+ */
+?>
+
+<!-- wp:pattern {"slug":"hrswds/banner-hero"} /-->
