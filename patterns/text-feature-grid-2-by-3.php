8d77eb5f2420d3cfe95562d33ae559968c79d618
Adam Turner <act@adamturner.dev>
2024-06-10 09:49:25 -0700

    Create feature grid 2 by 3 text pattern for #126

---
+++ patterns/text-feature-grid-2-by-3.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Title: Feature grid, 2 by 3 columns
+ * Slug: hrswds/text-feature-grid-2-by-3
+ * Categories: text, about
+ * Viewport width: 1400
+ * Description: A feature grid of 2 rows and 3 columns with headings and text.
+ *
+ * @package HRSWP_ThemeWDS
+ */
+
+namespace HRSWP\Theme\WDS\Patterns\TextFeatureGrid2By3;
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Feature grid"},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group">
+		<!-- wp:heading {"textAlign":"center","className":"is-style-callout"} -->
+		<h2 class="wp-block-heading has-text-align-center is-style-callout"><?php echo esc_html_x( 'A passion for helping', 'Heading of the features', 'hrswp-theme-wds' ); ?></h2>
+		<!-- /wp:heading -->
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?php echo esc_html_x( 'We provide efficient customer-focused solutions to help WSU faculty, staff, and students meet their goals.', 'Sub-heading of the features', 'hrswp-theme-wds' ); ?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+	<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+	<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide">
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+			<h3 class="wp-block-heading has-medium-font-size"><?php echo esc_html_x( 'Benefits', 'Sample feature heading', 'hrswp-theme-wds' ); ?></h3>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'WSU offers staff and faculty comprehensive benefits, professional development, and work/life quality programs.', 'Sample feature content', 'hrswp-theme-wds' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+			<h3 class="wp-block-heading has-medium-font-size"><?php echo esc_html_x( 'Time Off and Leave', 'Sample feature heading', 'hrswp-theme-wds' ); ?></h3>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'WSU offers staff and faculty comprehensive benefits, professional development, and work/life quality programs.', 'Sample feature content', 'hrswp-theme-wds' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+			<h3 class="wp-block-heading has-medium-font-size"><?php echo esc_html_x( 'Careers', 'Sample feature heading', 'hrswp-theme-wds' ); ?></h3>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'WSU offers staff and faculty comprehensive benefits, professional development, and work/life quality programs.', 'Sample feature content', 'hrswp-theme-wds' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+	<!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+	<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+			<h3 class="wp-block-heading has-medium-font-size"><?php echo esc_html_x( 'Learning and Development', 'Sample feature heading', 'hrswp-theme-wds' ); ?></h3>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'WSU offers staff and faculty comprehensive benefits, professional development, and work/life quality programs.', 'Sample feature content', 'hrswp-theme-wds' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+			<h3 class="wp-block-heading has-medium-font-size"><?php echo esc_html_x( 'Disability Services', 'Sample feature heading', 'hrswp-theme-wds' ); ?></h3>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'WSU offers staff and faculty comprehensive benefits, professional development, and work/life quality programs.', 'Sample feature content', 'hrswp-theme-wds' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":3,"fontSize":"medium"} -->
+			<h3 class="wp-block-heading has-medium-font-size"><?php echo esc_html_x( 'Recognition', 'Sample feature heading', 'hrswp-theme-wds' ); ?></h3>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html_x( 'WSU offers staff and faculty comprehensive benefits, professional development, and work/life quality programs.', 'Sample feature content', 'hrswp-theme-wds' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
