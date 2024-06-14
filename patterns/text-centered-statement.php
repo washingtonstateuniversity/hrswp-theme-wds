730707d7b384a1745d562771e2c62ff537f2c9dc
Adam Turner <act@adamturner.dev>
2024-06-14 11:07:45 -0700

    Create text pattern centered statement for #126

---
+++ patterns/text-centered-statement.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Centered statement
+ * Slug: hrswds/text-centered-statement
+ * Categories: text, about
+ * Keywords: introduction
+ * Viewport width: 1400
+ * Description: A centered text statement with a large paddings.
+ *
+ * @package HRSWP_ThemeWDS
+ */
+
+namespace HRSWP\Theme\WDS\Patterns\TextCenteredStatement;
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Centered statement"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"secondary-light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-secondary-light-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+	<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:spacer {"height":"var:preset|spacing|50"} -->
+		<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+
+		<!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"400"}},"fontSize":"x-large","fontFamily":"heading"} -->
+		<p class="has-text-align-center has-heading-font-family has-x-large-font-size" style="font-style:normal;font-weight:400;line-height:1.2"><?php echo esc_html_x( 'Rewarding careers at a public research university empowering students, faculty, and staff to create a thriving world.', 'Sample text', 'hrswp-theme-wds' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:spacer {"height":"var:preset|spacing|50"} -->
+		<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
